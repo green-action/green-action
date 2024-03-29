86dac26cbcd6665056fb2c6610cced01ab3ed10d
innes-k <innes.kwak@gmail.com>
2024-03-29 21:01:21 +0900

    [design] reset css

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={GeistSans.className}>
       <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
+        <main>
           <QueryProvider>{children}</QueryProvider>
         </main>
       </body>
