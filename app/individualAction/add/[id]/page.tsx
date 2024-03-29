03c83e1db794c976e0a67078e8672de573ccf182
innes-k <innes.kwak@gmail.com>
2024-03-29 21:12:17 +0900

    [test] test

--- app/individualAction/add/[id]/page.tsx
+++ app/individualAction/add/[id]/page.tsx
@@ -1,28 +1,9 @@
-// import React from "react";
+import React from "react";
 
-// const page = ({
-//   params,
-// }: {
-//   params: {
-//     id: string;
-//   };
-// }) => {
-//   const { id } = params;
-//   return (
-//     <div>
-//       {id === "" ? (
-//         <>
-//           <>add</>
-//           {/*  아니면 이걸 컴포넌트화 */}
-//         </>
-//       ) : (
-//         {id !== "" || id !==null && <><컴포넌트화 한거></컴포넌트화 한거></> }
-//         <>
-//           <>edit</>
-//         </>
-//       )}
-//     </div>
-//   );
-// };
+const Add = ({ params }: { params: { id: number } }) => {
+  const { id } = params;
+  console.log("id", id);
+  return <div>Add</div>;
+};
 
-// export default page;
+export default Add;
