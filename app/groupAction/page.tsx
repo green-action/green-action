ef0a2ad88e8e93f6e6ac355d71b5a1b8dde5d8e1
tjdwns335 <http://github.com/tjdwns335>
2024-04-02 10:57:08 +0900

    [add] GroupModal 추가

--- app/groupAction/page.tsx
+++ app/groupAction/page.tsx
@@ -1,20 +1,10 @@
 "use client";
 import React from "react";
-import { useGroupAction } from "../_hooks/useQueries";
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  useDisclosure,
-} from "@nextui-org/react";
+import { useGroupAction } from "../_hooks/useQueries/groupAction";
+import GroupModal from "../_components/groupAction/GroupModal";
 
 const groupActionPage = () => {
   const { data: groupAction, isLoading } = useGroupAction();
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
   if (isLoading || !groupAction) {
     return <div>Loading...</div>;
   }
@@ -30,28 +20,7 @@ const groupActionPage = () => {
           <div key={action.id}>
             <img src={action.img_url} alt="campaign Img" />
             <h2>캠페인 명 : {action.title}</h2>
-            <Button onPress={onOpen}>자세히</Button>
-            <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-              <ModalContent>
-                {(onClose) => (
-                  <>
-                    <ModalHeader className="flex flex-col gap-1">
-                      <img src={action.img_url} alt="캠페인 포스터" />
-                    </ModalHeader>
-                    <ModalBody>
-                      <h2>캠페인 명 : {action.title}</h2>
-                      <p>주관 : {action.hosted_by}</p>
-                      <p>상세내용 : {action.content}</p>
-                    </ModalBody>
-                    <ModalFooter>
-                      <Button color="danger" variant="light" onPress={onClose}>
-                        Close
-                      </Button>
-                    </ModalFooter>
-                  </>
-                )}
-              </ModalContent>
-            </Modal>
+            <GroupModal action={action} />
           </div>
         );
       })}
