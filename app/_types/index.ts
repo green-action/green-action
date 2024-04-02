4bc43b73e02ad4e11e43740eb2f0518c44143490
HY965 <hyeonj965@gmail.com>
2024-04-02 20:46:28 +0900

    feat: 오타수정

--- app/_types/index.ts
+++ app/_types/index.ts
@@ -1,5 +1,3 @@
-import { User as SupabaseUser } from "@supabase/auth-js";
-
 export type User = {
   id: string;
   email?: string | undefined;
@@ -9,13 +7,3 @@ export type User = {
   point: number;
   introduction: string;
 };
-
-// export interface User extends SupabaseUser {
-//   id: string;
-//   email: string;
-//   profile_img: string | null;
-//   display_name: string;
-//   password: string;
-//   point: number;
-//   introduction: string;
-// }
