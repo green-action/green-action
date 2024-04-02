f467bf3c95aaa2339d35b10bc107bef37e7dc5e5
tjdwns335 <http://github.com/tjdwns335>
2024-04-02 16:08:10 +0900

    [add] - bookmark임시

--- app/_components/bookmark/Bookmark.tsx
+++ app/_components/bookmark/Bookmark.tsx
@@ -1,27 +1,58 @@
+"use client";
 import { supabase } from "@/utils/supabase/client";
-import React, { useEffect, useState } from "react";
+import { CiStar } from "react-icons/ci";
+import { FaStar } from "react-icons/fa6";
 
-const Bookmark = async () => {
+const Bookmark = async ({ action_id }: { action_id: string }) => {
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  let { data: individualGreenActionsUid } = await supabase
-    .from("individual_green_actions")
-    .select("id");
+  let { data: bookmarks, error } = await supabase.from("bookmarks").select("*");
 
   const user_uid = user?.user_metadata.user_uid;
-  const action_id = individualGreenActionsUid;
-
-  useEffect(() => {}, []);
+  // 클릭시 user_uid추가 action_id추가
+  //  action_id받아서 table안에 action_id랑 일치 여부?
+  // 상태에 따른 랜더링
+  const handleAddBookmarkClick = async (
+    user_uid: string,
+    action_id: string,
+  ) => {
+    if (user_uid !== null) {
+      const { data, error } = await supabase
+        .from("bookmarks")
+        .insert([{ user_uid, action_id }])
+        .select();
+    }
+  };
+  const handleRemoveBookmarkClick = async (action_id: string) => {
+    const { error } = await supabase
+      .from("bookmarks")
+      .delete()
+      .eq(action_id, action_id);
+  };
 
   return (
     <>
-      {/* {
-    bookmark?.map((mark)=>{
-      mark.action_id === action_id && mark.user_uid === user_uid ?
-    })
-  } */}
+      {bookmarks?.map((mark) => {
+        {
+          mark.action_id === action_id ? (
+            <>
+              <button onClick={() => handleRemoveBookmarkClick(action_id)}>
+                <FaStar className="text-amber-300" />
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                onClick={() => handleAddBookmarkClick(user_uid, action_id)}
+              >
+                <CiStar />
+              </button>
+            </>
+          );
+        }
+      })}
     </>
   );
 };
---
+++ app/test/page.tsx
@@ -0,0 +1,11 @@
+import Bookmark from "../_components/bookmark/Bookmark";
+
+const TestPage = () => {
+  return (
+    <>
+      <Bookmark action_id="6f971b1e-abaf-49d5-90e7-f8c6bfe4bd58" />
+    </>
+  );
+};
+
+export default TestPage;
