e3c14785538c7be0f77f1c5042d2118b25a9da8d
tjdwns335 <http://github.com/tjdwns335>
2024-04-02 10:48:20 +0900

    [add] - usequery파일 옮김

--- app/_api/groupAction/getGroupAction.ts
+++ app/_api/groupAction/getGroupAction.ts
@@ -1,6 +1,4 @@
-import { QUERY_KEY_GROUPACTION } from "@/app/_api/queryKeys";
 import { supabase } from "@/utils/supabase/client";
-import { useQuery } from "@tanstack/react-query";
 
 export const getGroupAction = async () => {
   const { data: groupGreenActions, error } = await supabase
@@ -10,12 +8,4 @@ export const getGroupAction = async () => {
     throw new Error(error?.message || "에러가 발생했습니다.");
   }
   return { groupGreenActions, error };
-};
-
-export const useGroupAction = () => {
-  const { data, isLoading } = useQuery({
-    queryKey: [QUERY_KEY_GROUPACTION],
-    queryFn: getGroupAction,
-  });
-  return { data, isLoading };
 };
