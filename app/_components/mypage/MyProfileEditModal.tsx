f797d8adb55295b599bc2b31a14098c1bfcad2cd
Hyeryeon-Seo <0168968060@naver.com>
2024-04-04 18:23:25 +0900

    [feat] 마이페이지 프로필이미지 등록 구현

--- app/_components/mypage/MyProfileEditModal.tsx
+++ app/_components/mypage/MyProfileEditModal.tsx
@@ -1,6 +1,9 @@
+import {
+  insertProfileImgUrl,
+  uploadProfileFileAndGetUrl,
+} from "@/app/_api/mypage/mypage-profile-api";
 import { useUpdateUserName } from "@/app/_hooks/useMutations/mypage";
 import {
-  Avatar,
   Button,
   Input,
   Modal,
@@ -10,27 +13,34 @@ import {
   useDisclosure,
 } from "@nextui-org/react";
 import React, { useState } from "react";
-import { IoIosCamera } from "react-icons/io";
 import { TfiPencil } from "react-icons/tfi";
+import ProfileImgUpload from "./ProfileImgUpload";
 
 const MyProfileEditModal = ({
+  user_uid,
   display_name,
   profile_img,
 }: {
+  user_uid: string;
   display_name: string;
   profile_img: string;
 }) => {
   const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure();
 
   const [editedName, setEditedName] = useState<string>(display_name); // 초기값 닉네임 떴다가 안떴다가 함
+  const [uploadedFileUrl, setUploadedFileUrl] = useState<string>("");
+  const [file, setFile] = useState<File | undefined>();
 
   const { updateName } = useUpdateUserName(
     "ed71fea7-2892-4769-b7d0-1f8ba330c213", // user_uid
     editedName,
   );
 
+  // 모달창 '작성완료'없이 닫을 시 초기화
   const handleModalClose = () => {
     setEditedName(display_name);
+    setUploadedFileUrl("");
+    setFile(undefined);
   };
 
   const handleDisplayNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -41,18 +51,25 @@ const MyProfileEditModal = ({
     onOpen();
   };
 
-  // 모달 - 작성완료
+  // NOTE TODO 기존 이미지 업로드시 ?
+  // 모달창 - 작성완료
   const handleEditProfileSubmit = async (
     e: React.FormEvent<HTMLFormElement>,
   ) => {
     e.preventDefault();
+
     if (!editedName.trim()) {
-      return alert("닉네임을 입력해주세요."); //바꾸기?
+      return alert("닉네임을 입력해주세요."); // 바꾸기?
     }
     if (editedName.length >= 10) {
       return alert("닉네임을 10자 이내로 써주세요");
     }
 
+    // 이미지 stroage url 받아오기
+    const imgUrl = await uploadProfileFileAndGetUrl({ file, user_uid });
+    // 받아온 img url 을 users table에 업데이트
+    await insertProfileImgUrl({ user_uid, imgUrl });
+    // 닉네임 업데이트
     await updateName();
     onClose();
   };
@@ -84,12 +101,13 @@ const MyProfileEditModal = ({
                   <p className="text-[0.8rem] text-gray-600">
                     나중에 언제든지 변경할 수 있습니다.
                   </p>
-                  <Avatar
-                    showFallback
-                    src={profile_img || ""}
-                    className="w-[8rem] h-[8rem]"
+                  <ProfileImgUpload
+                    uploadedFileUrl={uploadedFileUrl}
+                    setUploadedFileUrl={setUploadedFileUrl}
+                    file={file}
+                    setFile={setFile}
+                    profile_img={profile_img}
                   />
-                  <IoIosCamera size="35" />
                   <label htmlFor="user-display-name">사용자 이름</label>
                   <Input
                     type="text"
@@ -110,9 +128,9 @@ const MyProfileEditModal = ({
                 </div>
                 <ModalFooter>
                   <Button color="danger" variant="light" onPress={onClose}>
-                    Close
+                    취소
                   </Button>
-                  <Button type="submit" color="success">
+                  <Button type="submit" variant="light" color="primary">
                     작성완료
                   </Button>
                 </ModalFooter>
