964af1c54bb5d1600924a82ae877622445af9f8b
KY <rudyeon@naver.com>
2024-04-07 00:40:17 +0900

    [design] 상세페이지 작성자 부분

--- app/individualAction/detail/[id]/page.tsx
+++ app/individualAction/detail/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   useActionImages,
   useIndividualAction,
 } from "@/app/_hooks/useQueries/individualActions";
-import { CircularProgress } from "@nextui-org/react";
+import { Avatar, CircularProgress } from "@nextui-org/react";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import { useParams } from "next/navigation";
 import React from "react";
@@ -13,8 +13,13 @@ import { LuMapPin } from "react-icons/lu";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useFetchUserInfo } from "@/app/_hooks/useQueries/mypage";
+import { useSession } from "next-auth/react";
+import { User } from "@/app/_types";
 
 const DetailPage = () => {
+  const session = useSession();
+  const user_uid = session?.data?.user.user_uid || "";
   const settings = {
     dots: true,
     infinite: true,
@@ -22,6 +27,9 @@ const DetailPage = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+
+  const { data, isLoading: fetchUserInfoLoading } = useFetchUserInfo(user_uid);
+  const { display_name, introduction, profile_img } = (data as User) || "";
 
   const { id: postId } = useParams<Params>();
   const params = { id: postId };
@@ -31,7 +39,7 @@ const DetailPage = () => {
     isError: individualActionError,
   } = useIndividualAction({ params });
 
-  // 이미지 가져오기
+  // 상세 이미지 가져오기
   const {
     data: imgUrl,
     isLoading: actionImagesLoading,
@@ -39,34 +47,44 @@ const DetailPage = () => {
   } = useActionImages({ params });
 
   // 두 요청이 모두 완료되었는지 확인
-  const isLoading = individualActionLoading || actionImagesLoading;
+  const isLoading =
+    individualActionLoading || actionImagesLoading || fetchUserInfoLoading;
   const isError = individualActionError || actionImagesError;
 
   console.log("이미지url : ", imgUrl);
 
   if (isLoading || !individualAction)
     return (
-      <div>
+      <div className="flex justify-center items-center h-screen">
         <CircularProgress color="success" aria-label="Loading..." />
       </div>
     );
   const detail = individualAction![0];
   console.log(detail);
 
-  // 작성자 이름, 자기소개 가져오기
-  // const userId = detail.user_uid;
-  // userId를 가진 유저정보를 auth에서 가져와서
-
   if (isError) return <div>Error fetching details...</div>;
   return (
     <>
-      <div>개인그린디테일page</div>
       <div className="flex flex-col w-[1200px] h-auto mx-auto m-5">
         <div className="grid grid-cols-4 grid-rows-4 gap-4">
           <div className="col-span-1 row-span-1 border-2 border-gray-300 rounded-3xl">
-            작성자 : {detail.users?.display_name}
-            <hr />
-            {detail.users?.introduction}
+            <div className="mt-5 ml-5 mr-5 mb-3 border-b-2 border-b-gray-300 content-center">
+              <div className="flex gap-4 items-center">
+                <Avatar
+                  showFallback
+                  src={profile_img || ""}
+                  className="w-[2.5rem] h-[2.5rem]"
+                />
+                <div className="flex flex-col gap-[0.1rem] w-[9rem]">
+                  <p className="text-[0.7rem]">작성자</p>
+                  <p className="font-bold text-sm">{display_name} Greener</p>
+                </div>
+              </div>
+              <div className="flex items-end pb-2"></div>
+            </div>
+            <div className="ml-5">
+              <p className="text-[0.8rem]">{introduction}</p>
+            </div>
           </div>
           <div className="col-span-3 row-span-1 border-2 border-gray-300 rounded-3xl">
             <p>제목 : {detail.title}</p>
@@ -88,9 +106,12 @@ const DetailPage = () => {
               참여하기
             </div>
           </div>
-          <div className="col-span-3 row-span-3 border-2 border-gray-300 rounded-3xl flex justify-center pt-20">
-            <p>상세내용 : {detail.content}</p>
-
+          <div className="col-span-3 row-span-3 border-2 border-gray-300 rounded-3xl flex justify-around pt-12">
+            <div>
+              <p className="text-neutral-600">상세내용</p>
+              <br />
+              <p className="text-neutral-600">{detail.content}</p>
+            </div>
             {/* 이미지 슬라이드 */}
             <Slider {...settings} className="w-[250px] h-[250px]">
               {imgUrl?.map((item) => {
