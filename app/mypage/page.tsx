4213b1ae18571a1606f59d698f625cc88b11f1b6
Hyeryeon-Seo <0168968060@naver.com>
2024-04-08 01:52:57 +0900

    [fix] 마이페이지 작성게시물 유저정보 안뜨는 문제 해결 (CommunityListPost 컴포넌트수정)

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from "react";
 import MyProfile from "../_components/mypage/MyProfile";
 import {
   useFetchMyGreenActions,
+  useFetchUserInfo,
   usefetchBookmarkedActions,
   usefetchMyCommunityPosts,
 } from "../_hooks/useQueries/mypage";
@@ -15,19 +16,20 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import RecruitSelectTab from "../_components/mypage/RecruitSelectTab";
 import CommunityListPost from "../_components/community/CommunityListPost";
+import { User } from "../_types";
 
 // 로그인 안 한 상태에서 접근 차단할 것 -
 // FIXME  모집상태별 분류 다시 안됨
 const MyPage = () => {
   // TODO props 타입등 재설정
   // FIXME 유저닉네임 수정 다시 봐야
-  // const user_uid = "2c81257f-e4d9-41d8-ad65-3745da3d3b2f";
+  const user_uid = "2c81257f-e4d9-41d8-ad65-3745da3d3b2f";
   // 임시 유저 아이디 설정
   const router = useRouter();
   const session = useSession();
 
   const isLoggedIn = !!session.data;
-  const user_uid = session.data?.user.user_uid as string;
+  // const user_uid = session.data?.user.user_uid as string;
   // let user_uid = "";
   // 새로고침 시 로그인이 잠시 풀림
   // if (!isLoggedIn) {
@@ -72,6 +74,12 @@ const MyPage = () => {
 
   const { data: myBookmarks, isLoading: isBookmarksLoading } =
     usefetchBookmarkedActions(user_uid);
+
+  // 유저 정보 조회
+  const { data: userInfo, isLoading: isUserInfoLoading } =
+    useFetchUserInfo(user_uid);
+
+  const { display_name, profile_img } = (userInfo as User) || ""; // as User 외에도 || '' 처리해줘야 에러안뜸
 
   // my action - created_at (작성일) 기준으로 정렬하기
   const sortedMyActions = myActions?.slice().sort((a, b) => {
@@ -156,7 +164,12 @@ const MyPage = () => {
     }
   };
 
-  if (isActionsLoading || isPostsLoading || isBookmarksLoading) {
+  if (
+    isActionsLoading ||
+    isPostsLoading ||
+    isBookmarksLoading ||
+    isUserInfoLoading
+  ) {
     return (
       <div className="flex justify-center items-center h-screen">
         <CircularProgress color="success" label="Loading..." />
@@ -166,13 +179,8 @@ const MyPage = () => {
 
   return (
     <div className="flex justify-center pt-12 mb-[100px]">
-      {/* <CustomConfirm
-        text="안녕"
-        buttonName="버튼"
-        okFunction={() => setFilteredActions}
-      /> */}
       <div className="flex w-[1400px]">
-        <MyProfile user_uid={user_uid} />
+        <MyProfile userInfo={userInfo as User} />
         <div className="flex flex-col gap-10 pl-10 pt-1 w-full">
           <div className="flex justify-between">
             <div className="flex gap-12 ml-5">
@@ -225,13 +233,25 @@ const MyPage = () => {
             {activeTab === "My Green-Action" &&
               filteredActions?.map((action) => {
                 return (
-                  <MyActionCard key={action.id} action={action} mode="mypost" />
+                  <MyActionCard
+                    key={action.id}
+                    action={action}
+                    mode="myPosts"
+                  />
                 );
               })}
             {/* LINK 내가 쓴 커뮤니티 글 */}
             {activeTab === "작성 게시물" &&
               myPosts?.map((post) => {
-                return <CommunityListPost mode="mypost" communityPost={post} />;
+                return (
+                  <CommunityListPost
+                    key={post.id}
+                    mode="myPosts"
+                    communityPost={post}
+                    my_display_name={display_name}
+                    my_profile_img={profile_img || null}
+                  />
+                );
               })}
             {/* LINK 찜한 Green Action */}
             {activeTab === "찜한 Green-Action" &&
@@ -240,7 +260,7 @@ const MyPage = () => {
                   <MyActionCard
                     key={bookmark?.bookmarkedAction?.id || ""}
                     action={bookmark}
-                    mode="bookmark"
+                    mode="myBookmarks"
                   />
                 );
               })}
