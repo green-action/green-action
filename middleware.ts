da5606451af6a36070a401a6af29da3cb5dc1db9
tjdwns335 <http://github.com/tjdwns335>
2024-04-08 10:11:04 +0900

    [add] - middiware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,49 @@
 import { updateSession } from "@/utils/supabase/middleware";
+import { getSession } from "next-auth/react";
 import { type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  console.log("-----------------");
+  console.log(session);
+  console.log("-----------------");
+
+  // 로그인이 필요한 페이지 리스트
+  const LOGIN_REQUIRED_PAGES = [
+    `/mypage`,
+    `/community`,
+    `/individualAction`,
+    `/goods`,
+  ];
+  const LOGIN_NOT_REQUIRED_PAGES = [`/login`, `/signup`];
+
+  // 로그인이 필요한 페이지 리스트
+  LOGIN_REQUIRED_PAGES.forEach((page) => {
+    if (request.nextUrl.pathname === page) {
+      if (!session) {
+        return {
+          status: 302,
+          headers: {
+            location: `/login`,
+          },
+        };
+      }
+    }
+  });
+
+  LOGIN_NOT_REQUIRED_PAGES.forEach((page) => {
+    if (request.nextUrl.pathname === page) {
+      if (session) {
+        return {
+          status: 302,
+          headers: {
+            location: `/`,
+          },
+        };
+      }
+    }
+  });
+
   return await updateSession(request);
 }
 
