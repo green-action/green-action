815e61605a4a8a61360e9058f6167ed66dedb00a
tjdwns335 <http://github.com/tjdwns335>
2024-04-08 16:52:27 +0900

    [add] - like debounce 로직 추가

---
+++ utils/debounce/debounce.ts
@@ -0,0 +1,10 @@
+export const debounce = (callback: () => void, delay: number) => {
+  type timerId = ReturnType<typeof setTimeout> | null;
+  let timerId: timerId = null;
+  return (...args: []) => {
+    if (timerId) clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+};
