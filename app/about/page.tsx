7b4af0ff5d244117a7bbd5241838d82f551df57d
innes-k <innes.kwak@gmail.com>
2024-04-10 19:10:12 +0900

    [design] about페이지 타이틀 이미지 추가

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import titleImg from "../../app/_assets/image/about/1.png";
+import Image from "next/image";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="relative">
+      <Image
+        src={titleImg}
+        alt="about title image"
+        className="relative top-0 w-[95%] h-[1090px] object-cover mx-auto rounded-b-[60px] brightness-50"
+      />
+      <div className="absolute bottom-60 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center bottom-12 text-white text-[64px] font-thin">
+        <p className="">Experience the earth breathing</p>
+        <p className="">together in your daily life</p>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
