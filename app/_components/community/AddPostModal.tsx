f427c3a43efad6c212d6f238ea128c6770fc2cb9
innes-k <innes.kwak@gmail.com>
2024-04-10 16:32:19 +0900

    [design] 게시물 글쓰기 모달 - 디자인 수정

--- app/_components/community/AddPostModal.tsx
+++ app/_components/community/AddPostModal.tsx
@@ -5,6 +5,9 @@ import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 
 import { uploadFileAndGetUrl } from "@/app/_api/community/community-api";
+import { useInsertCommunityPostFormData } from "@/app/_hooks/useMutations/community";
+
+import PostImgUpload from "./PostImgUpload";
 
 import {
   Button,
@@ -22,8 +25,6 @@ import {
 } from "@nextui-org/react";
 
 import { LuPencilLine } from "react-icons/lu";
-import { useInsertCommunityPostFormData } from "@/app/_hooks/useMutations/community";
-import PostImgUpload from "./PostImgUpload";
 
 const AddPostModal = () => {
   const [uploadedFileUrl, setUploadedFileUrl] = useState<string>("");
@@ -68,6 +69,15 @@ const AddPostModal = () => {
     event.preventDefault();
 
     const formData = new FormData(event.target as HTMLFormElement);
+
+    // if (
+    //   !formData.get("activityTitle") ||
+    //   !formData.get("activityDescription")
+    // ) {
+    //   alert("입력하신 내용이 없습니다.");
+    //   return;
+    // }
+
     // 드롭다운에서 선택한 값을 formData에 추가
     formData.append("action_type", Array.from(selectedKeys).join(", "));
 
@@ -116,11 +126,11 @@ const AddPostModal = () => {
         <LuPencilLine className="w-8 h-8" />
       </Button>
       {/* 게시글 글쓰기 모달창 */}
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent className="h-[600px]">
+      <Modal size="lg" isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent className="h-[740px]">
           {(onClose) => (
             <form onSubmit={handleSubmit}>
-              <ModalHeader className="flex items-center py-3 px-6 font-medium text-xs">
+              <ModalHeader className="flex items-center py-3 px-6 font-extrabold text-[15px]">
                 New Post
               </ModalHeader>
               <hr className="border-t-1 border-gray-300" />
@@ -134,12 +144,12 @@ const AddPostModal = () => {
                   />
                   <div className="flex flex-col gap-3">
                     {/* action_type선택 드랍다운 */}
-                    <div className="flex justify-end">
+                    <div className="flex justify-end mr-3">
                       <Dropdown>
                         <DropdownTrigger>
                           <Button
                             variant="bordered"
-                            className="capitalize border-1 rounded-full h-7"
+                            className="capitalize border-1 rounded-full h-8 text-gray-500"
                           >
                             {selectedValue}
                           </Button>
@@ -152,20 +162,26 @@ const AddPostModal = () => {
                           selectedKeys={selectedKeys}
                           onSelectionChange={setSelectedKeys}
                         >
-                          <DropdownItem key="개인과 함께해요">
+                          <DropdownItem
+                            key="개인과 함께해요"
+                            className="text-gray-500"
+                          >
                             개인과 함께해요
                           </DropdownItem>
-                          <DropdownItem key="단체와 함께해요">
+                          <DropdownItem
+                            key="단체와 함께해요"
+                            className="text-gray-500"
+                          >
                             단체와 함께해요
                           </DropdownItem>
                         </DropdownMenu>
                       </Dropdown>
                     </div>
                     {/* 활동 제목 */}
-                    <div className="flex mx-auto w-full h-[34px] items-center pl-8 border-1 border-gray-300 rounded-3xl">
+                    <div className="flex mx-auto w-[95%] h-[42px] items-center pl-8 border-1 border-gray-300 rounded-3xl">
                       <label
                         htmlFor="activityTitle"
-                        className="text-xs font-semibold w-[61px]"
+                        className="text-sm text-gray-500 font-semibold w-[61px]"
                       >
                         활동 제목
                       </label>
@@ -178,10 +194,10 @@ const AddPostModal = () => {
                       />
                     </div>
                     {/* 활동 내용 */}
-                    <div className="flex items-start w-full h-auto pl-8 border-1 border-gray-300 rounded-3xl mb-8">
+                    <div className="flex items-start flex-col w-[95%] h-auto pl-8 border-1 border-gray-300 rounded-3xl mb-3.5 mx-auto">
                       <label
                         htmlFor="activityDescription"
-                        className="text-xs font-semibold w-[61px] mt-3"
+                        className="text-sm text-gray-500 font-semibold w-[61px] mt-4"
                       >
                         활동 내용
                       </label>
@@ -189,25 +205,25 @@ const AddPostModal = () => {
                         id="activityDescription"
                         name="activityDescription"
                         required
-                        className="resize-none w-10/12 h-[100px] mx-4 mt-2 pr-4 bg-inherit focus:outline-none text-sm text-gray-400"
+                        className="resize-none w-11/12 h-[150px] mt-3 bg-inherit focus:outline-none text-sm text-gray-400"
                       />
                     </div>
                   </div>
                 </div>
               </ModalBody>
               {/* 취소, 작성 버튼 */}
-              <ModalFooter className="flex justify-center mb-8 !p-0">
-                <Button
+              <ModalFooter className="flex justify-center mb-12 !p-0">
+                {/* <Button
                   variant="light"
                   onPress={onClose}
                   className="rounded-full !w-[110px] h-[27px] border-1"
                 >
                   취소하기
-                </Button>
+                </Button> */}
                 <Button
                   type="submit"
                   onPress={onClose}
-                  className="rounded-full !w-[110px] h-[27px]"
+                  className="text-gray-500 rounded-full !w-[140px] h-[33px] border border-gray-400 bg-[#EFEFEF]"
                 >
                   작성완료
                 </Button>
