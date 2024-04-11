a831dd68cf88acbfd5b7fe8fda59f14adfdf1023
innes-k <innes.kwak@gmail.com>
2024-04-11 21:08:06 +0900

    [fix] 그라데이션 문제 해결 (tailwind config backgroundImg 삽입 위치이동)

--- app/_components/community/CommunityListPost.tsx
+++ app/_components/community/CommunityListPost.tsx
@@ -67,7 +67,7 @@ const CommunityListPost = ({
               className="object-cover w-full h-full cursor-pointer brightness-90"
               src={communityPost?.img_url}
             />
-            <div className="absolute bottom-0 left-0 w-full h-16 bg-gradient-to-t from-[#6d6d6d] to-transparent"></div>
+            <div className="absolute bottom-0 left-0 w-full h-16 bg-gradient-to-t"></div>
           </div>
           <div className="flex items-center justify-between pl-[24px] pr-[16px] absolute bottom-0 text-white w-full h-[66px]">
             <div className="flex items-center">
