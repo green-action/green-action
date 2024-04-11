cbaac6cd4012d9c214a7267bec46413e3f2dec2a
tjdwns335 <http://github.com/tjdwns335>
2024-04-11 21:26:28 +0900

    [add] - cummunity/add 컴펌창 추가

--- app/_components/community/AddPostModal.tsx
+++ app/_components/community/AddPostModal.tsx
@@ -25,17 +25,19 @@ import {
 } from "@nextui-org/react";
 
 import { LuPencilLine } from "react-icons/lu";
+import CustomConfirm from "../customConfirm/CustomConfirm";
 
 const AddPostModal = () => {
   const [uploadedFileUrl, setUploadedFileUrl] = useState<string>("");
   const [file, setFile] = useState<File | undefined | null>(null);
+  const [isButton, setIsButton] = useState(true); // true가 비활성화 false가 활성화
 
   const router = useRouter();
 
   const ModalRef = useRef(null);
 
   // 게시글 글쓰기 모달창 open여부 상태관리
-  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   // 현재 로그인한 유저 uid
   const session = useSession();
@@ -72,14 +74,6 @@ const AddPostModal = () => {
 
     const formData = new FormData(event.target as HTMLFormElement);
 
-    if (
-      !formData.get("activityTitle") ||
-      !formData.get("activityDescription")
-    ) {
-      alert("입력하신 내용이 없습니다.");
-      return;
-    }
-
     // 드롭다운에서 선택한 값을 formData에 추가
     formData.append("action_type", Array.from(selectedKeys).join(", "));
 
@@ -89,7 +83,17 @@ const AddPostModal = () => {
       if (isConfirmed) {
         if (!file) {
           alert("사진은 필수값입니다.");
+          setIsButton(true);
           return;
+        } else if (
+          !formData.get("activityTitle") ||
+          !formData.get("activityDescription")
+        ) {
+          alert("입력하신 내용이 없습니다.");
+          setIsButton(true);
+          return;
+        } else {
+          setIsButton(false);
         }
 
         // 이미지 스토리지 업로드 후 url 반환받기
@@ -118,25 +122,6 @@ const AddPostModal = () => {
     }
   };
 
-  const handleClick = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.target as HTMLFormElement);
-
-    if (
-      !formData.get("activityTitle") ||
-      !formData.get("activityDescription")
-    ) {
-      alert("입력하신 내용이 없습니다.");
-      return;
-    } else if (!file) {
-      alert("사진은 필수값입니다.");
-      return;
-    } else {
-      return onClose();
-    }
-  };
-
   return (
     <>
       {/* 글쓰기 버튼 */}
@@ -153,108 +138,116 @@ const AddPostModal = () => {
         isOpen={isOpen}
         onOpenChange={onOpenChange}
       >
-        <ModalContent className="h-[740px]">
+        <ModalContent className="h-[740px] relative">
           {(onClose) => (
-            <form onSubmit={handleSubmit}>
-              <ModalHeader className="flex items-center py-3 px-6 font-extrabold text-[15px]">
-                New Post
-              </ModalHeader>
-              <hr className="border-t-1 border-gray-300" />
-              <ModalBody>
-                <div className="flex flex-col justify-between h-full">
-                  {/* 이미지 업로드 */}
-                  <PostImgUpload
-                    uploadedFileUrl={uploadedFileUrl}
-                    setUploadedFileUrl={setUploadedFileUrl}
-                    setFile={setFile}
-                  />
-                  <div className="flex flex-col gap-3">
-                    {/* action_type선택 드랍다운 */}
-                    <div className="flex justify-end mr-3">
-                      <Dropdown>
-                        <DropdownTrigger>
-                          <Button
-                            variant="bordered"
-                            className="capitalize border-1 rounded-full h-8 text-gray-500"
+            <>
+              <CustomConfirm
+                text="취소 하시겠습니까?"
+                mode="community"
+                okFunction={onClose}
+              />
+              <form onSubmit={handleSubmit}>
+                <ModalHeader className="flex items-center py-3 px-6 font-extrabold text-[15px]">
+                  New Post
+                </ModalHeader>
+                <hr className="border-t-1 border-gray-300" />
+                <ModalBody>
+                  <div className="flex flex-col justify-between h-full">
+                    {/* 이미지 업로드 */}
+                    <PostImgUpload
+                      uploadedFileUrl={uploadedFileUrl}
+                      setUploadedFileUrl={setUploadedFileUrl}
+                      setFile={setFile}
+                    />
+                    <div className="flex flex-col gap-3">
+                      {/* action_type선택 드랍다운 */}
+                      <div className="flex justify-end mr-3">
+                        <Dropdown>
+                          <DropdownTrigger>
+                            <Button
+                              variant="bordered"
+                              className="capitalize border-1 rounded-full h-8 text-gray-500"
+                            >
+                              {selectedValue}
+                            </Button>
+                          </DropdownTrigger>
+                          <DropdownMenu
+                            aria-label="Single selection example"
+                            variant="flat"
+                            disallowEmptySelection
+                            selectionMode="single"
+                            selectedKeys={selectedKeys}
+                            onSelectionChange={setSelectedKeys}
                           >
-                            {selectedValue}
-                          </Button>
-                        </DropdownTrigger>
-                        <DropdownMenu
-                          aria-label="Single selection example"
-                          variant="flat"
-                          disallowEmptySelection
-                          selectionMode="single"
-                          selectedKeys={selectedKeys}
-                          onSelectionChange={setSelectedKeys}
+                            <DropdownItem
+                              key="개인과 함께해요"
+                              className="text-gray-500"
+                            >
+                              개인과 함께해요
+                            </DropdownItem>
+                            <DropdownItem
+                              key="단체와 함께해요"
+                              className="text-gray-500"
+                            >
+                              단체와 함께해요
+                            </DropdownItem>
+                          </DropdownMenu>
+                        </Dropdown>
+                      </div>
+                      {/* 활동 제목 */}
+                      <div className="flex mx-auto w-[95%] h-[42px] items-center pl-8 border-1 border-gray-300 rounded-3xl">
+                        <label
+                          htmlFor="activityTitle"
+                          className="text-sm text-gray-500 font-semibold w-[61px]"
                         >
-                          <DropdownItem
-                            key="개인과 함께해요"
-                            className="text-gray-500"
-                          >
-                            개인과 함께해요
-                          </DropdownItem>
-                          <DropdownItem
-                            key="단체와 함께해요"
-                            className="text-gray-500"
-                          >
-                            단체와 함께해요
-                          </DropdownItem>
-                        </DropdownMenu>
-                      </Dropdown>
-                    </div>
-                    {/* 활동 제목 */}
-                    <div className="flex mx-auto w-[95%] h-[42px] items-center pl-8 border-1 border-gray-300 rounded-3xl">
-                      <label
-                        htmlFor="activityTitle"
-                        className="text-sm text-gray-500 font-semibold w-[61px]"
-                      >
-                        활동 제목
-                      </label>
-                      <input
-                        type="text"
-                        id="activityTitle"
-                        name="activityTitle"
-                        required
-                        className="w-10/12 h-[30px] mx-4 pr-4 bg-inherit focus:outline-none text-sm text-gray-400"
-                      />
-                    </div>
-                    {/* 활동 내용 */}
-                    <div className="flex items-start flex-col w-[95%] h-auto pl-8 border-1 border-gray-300 rounded-3xl mb-3.5 mx-auto">
-                      <label
-                        htmlFor="activityDescription"
-                        className="text-sm text-gray-500 font-semibold w-[61px] mt-4"
-                      >
-                        활동 내용
-                      </label>
-                      <textarea
-                        id="activityDescription"
-                        name="activityDescription"
-                        required
-                        className="resize-none w-11/12 h-[150px] mt-3 bg-inherit focus:outline-none text-sm text-gray-400"
-                      />
+                          활동 제목
+                        </label>
+                        <input
+                          type="text"
+                          id="activityTitle"
+                          name="activityTitle"
+                          required
+                          className="w-10/12 h-[30px] mx-4 pr-4 bg-inherit focus:outline-none text-sm text-gray-400"
+                        />
+                      </div>
+                      {/* 활동 내용 */}
+                      <div className="flex items-start flex-col w-[95%] h-auto pl-8 border-1 border-gray-300 rounded-3xl mb-3.5 mx-auto">
+                        <label
+                          htmlFor="activityDescription"
+                          className="text-sm text-gray-500 font-semibold w-[61px] mt-4"
+                        >
+                          활동 내용
+                        </label>
+                        <textarea
+                          id="activityDescription"
+                          name="activityDescription"
+                          required
+                          className="resize-none w-11/12 h-[150px] mt-3 bg-inherit focus:outline-none text-sm text-gray-400"
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
-              </ModalBody>
-              {/* 취소, 작성 버튼 */}
-              <ModalFooter className="flex justify-center mb-12 !p-0">
-                {/* <Button
+                </ModalBody>
+                {/* 취소, 작성 버튼 */}
+                <ModalFooter className="flex justify-center mb-12 !p-0">
+                  {/* <Button
                   variant="light"
                   onPress={onClose}
                   className="rounded-full !w-[110px] h-[27px] border-1"
                 >
                   취소하기
                 </Button> */}
-                <Button
-                  type="submit"
-                  onClick={() => handleClick}
-                  className="text-gray-500 rounded-full !w-[140px] h-[33px] border border-gray-400 bg-[#EFEFEF]"
-                >
-                  작성완료
-                </Button>
-              </ModalFooter>
-            </form>
+                  <Button
+                    type="submit"
+                    isDisabled={isButton}
+                    onPress={onClose}
+                    className="text-gray-500 rounded-full !w-[140px] h-[33px] border border-gray-400 bg-[#EFEFEF]"
+                  >
+                    작성완료
+                  </Button>
+                </ModalFooter>
+              </form>
+            </>
           )}
         </ModalContent>
       </Modal>
