a3c41e3149a95fa36e21f9afb59898441c7bd570
innes-k <innes.kwak@gmail.com>
2024-04-11 17:14:25 +0900

    [chore] about페이지 주석 정리

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 import Link from "next/link";
 
 import titleImg from "../../app/_assets/image/about/1.png";
-import textBg1 from "../../app/_assets/image/about/2.png";
-import textBg2 from "../../app/_assets/image/about/3.png";
+import cardBg1 from "../../app/_assets/image/about/2.png";
+import cardBg2 from "../../app/_assets/image/about/3.png";
 import mainImg from "../../app/_assets/image/about/main.png";
 
 import DynamicHeader from "../_components/layout/DynamicHeader";
@@ -25,7 +25,6 @@ const AboutTest = () => {
             alt="about title image"
             className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[95%] h-[1090px] object-cover mx-auto mb-[282px] rounded-b-[60px] brightness-[.4]"
           />
-          {/* <div className="absolute z-0 bottom-[60px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center text-white text-[64px] font-thin"> */}
           <div className=" z-0 mt-[500px] flex flex-col items-center text-white text-[64px] font-thin">
             <p className="font-['Italiana']">Experience the earth breathing</p>
             <p className="font-['Italiana']">together in your daily life</p>
@@ -33,33 +32,6 @@ const AboutTest = () => {
         </div>
         <div className="mt-[700px] flex flex-col items-center">
           {/* second part - 설명글 */}
-
-          {/* text-with-background 예시 */}
-          {/* 문제 : 이미지 어둡게 처리했을 시 글씨도 같이 어두워짐 */}
-          {/* <div className="flex items-center justify-center mx-auto w-[75%]">
-            <div className="text-with-background bg-text-bg-1 bg-cover bg-center w-[400px] text-center text-3xl rounded-full py-2">
-              <p className="text-white">일상의 행복은 어디에나</p>
-            </div>
-            <div>
-              <p className="text-3xl">
-                있습니다 일상의 행복은 어디에나일상의 행복은
-              </p>
-            </div>
-          </div> */}
-
-          {/* p태그로 줄바꿈, 가운데정렬 가능 예시 */}
-          {/* <div className="w-[75%] flex text-center">
-            <p className="text-wrap text-4xl inline-block">
-              우와 어쩌고저꺼고우와 어쩌고저꺼고우와 어쩌고저꺼고우와
-              어쩌고저꺼고우와 어쩌고저꺼고우와
-              <p className="inline-block text-with-background bg-text-bg-1 bg-cover bg-center rounded-full w-[600px] h-[50px]">
-                일상의 행복
-              </p>
-              <p className="inline-block">어쩌고저꺼고우와 어쩌고저꺼고</p>
-            </p>
-          </div> */}
-
-          {/* text background로 수정후 */}
           <div className="w-[75%] flex flex-col justify-center text-center text-[36px] mb-[420px] font-bold">
             {/* 첫 줄 */}
             <div>
@@ -100,62 +72,6 @@ const AboutTest = () => {
               </p>
             </div>
           </div>
-
-          {/* 기존 */}
-          {/* 문제 : 가운제정렬을 하면 (justify-center) 화면 % 줄일시 absolute 안의 글자가 이미지 밖으로 삐져나감 */}
-          {/* <div className="mx-auto w-[75%] mb-[420px]">
-            <div className="text-[36px]">
-              <div className="flex items-center relative">
-                <Image
-                  src={textBg1}
-                  alt="text-bg-1"
-                  className="w-[444px] h-[60px] object-cover rounded-full"
-                />
-                <span className="absolute text-white left-4">
-                  '일상에서 쉽게 실천할 수 있는
-                </span>
-                <span>
-                  환경 캠페인'은 지구를 지키는 우리의 작은 노력으로부터
-                  출발합니다.
-                </span>
-              </div>
-              <div className="flex flex-wrap items-center">
-                <span>당신의 일상에서 환경을 지키는 </span>
-                <div className="flex items-center relative h-[60px]">
-                  <Image
-                    src={textBg2}
-                    alt="text-bg-1"
-                    className="w-[444px] h-[60px] object-cover rounded-full"
-                  />
-                  <span className="absolute text-white left-4">
-                    작은 습관들이 모여 큰 변화를
-                  </span>
-                </div>
-                <span className="text-wrap">
-                  이끌어낼 수 있습니다. 이제 우리의 캠페인에 참여하여 당신도
-                  환경보호의 일원이 되어보세요. 우리의 목표는 모든 사람이 쉽게
-                  실천할 수 있는 환경 보호 방법을 제시하고, 그것들을 실제
-                  행동으로 이어지게 하는 것입니다. 이제 당신의 일상을 환경
-                  보호의 일부로 만들어보세요. 지금 바로
-                </span>
-              </div>
-
-              <div className="flex items-center relative h-[60px]">
-                <Image
-                  src={mainImg}
-                  alt="text-bg-1"
-                  className="w-[370px] h-[60px] object-cover rounded-full"
-                />
-                <span className="absolute text-white left-4">
-                  '지구가 숨쉬다. SOOM'
-                </span>
-              </div>
-              <span>
-                에 참여하여 우리의 지구를 함께 지켜 나가는 데 기여하세요. 당신의
-                작은 노력이 큰 변화를 만들어낼 수 있습니다. 함께해요!
-              </span>
-            </div>
-          </div> */}
           {/* third part - 카드 4장 */}
           <div className="flex flex-col w-[80%] h-[1344px] mx-auto items-center mb-[730px]">
             <div>
@@ -174,7 +90,7 @@ const AboutTest = () => {
                 {/* 카드 2 */}
                 <div className="w-[450px] h-[590px] rounded-[30px] mt-[140px] mb-[30px] relative">
                   <Image
-                    src={textBg1}
+                    src={cardBg1}
                     alt="card1"
                     className="w-full h-full object-cover rounded-[40px] brightness-[.8]"
                   />
@@ -195,7 +111,7 @@ const AboutTest = () => {
                 {/* 카드 3 */}
                 <div className="bg-white w-[450px] h-[590px] rounded-[40px] ml-[360px] mr-[40px] relative">
                   <Image
-                    src={textBg2}
+                    src={cardBg2}
                     alt="card1"
                     className="w-full h-full object-cover rounded-[40px] brightness-[.8]"
                   />
