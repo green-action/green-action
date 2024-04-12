4245d637ee8042fe210561914d75ab6809c9a16f
tjdwns335 <http://github.com/tjdwns335>
2024-04-12 14:08:03 +0900

    [add] - community 추가하면 포인트모달 창이 나옴

--- app/_components/community/PointModal.tsx
+++ app/_components/community/PointModal.tsx
@@ -1,40 +1,36 @@
 "use client";
 
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  useDisclosure,
-} from "@nextui-org/react";
+import { Modal, ModalBody, ModalContent } from "@nextui-org/react";
 import Image from "next/image";
 import clap from "/app/_assets/image/logo_icon/icon/community/image 50.png";
 
-const PointModal = () => {
-  const { isOpen, onOpenChange } = useDisclosure();
+interface PointModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const PointModal: React.FC<PointModalProps> = ({ isOpen, onClose }) => {
   return (
     <>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+      <Modal placement="center" isOpen={isOpen} onOpenChange={onClose}>
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalBody className="flex flex-col gap-5 justify-center items-center">
-                <Image src={clap} alt="clap" />
-                <p>
+              <ModalBody className="flex flex-col gap-5 justify-center items-center py-[70px]">
+                <Image
+                  className="block w-[100px] h-[80px]"
+                  src={clap}
+                  alt="clap"
+                />
+                <p className="font-bold text-center text-[16px]">
                   축하합니다!
                   <span className="block">100 Point를 획득하셨습니다!</span>
                 </p>
-                <p>
+                <p className="text-[#8f8f8f] text-[12px] text-center">
                   내용과 관련 없는 이미지일 경우
                   <br /> 포인트가 환수될 수 있습니다.
                 </p>
               </ModalBody>
-              <ModalFooter>
-                <Button variant="light" onPress={onClose}>
-                  Close
-                </Button>
-              </ModalFooter>
             </>
           )}
         </ModalContent>
