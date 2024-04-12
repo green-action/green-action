3684028f14f0b34e0e508d419daed804273b1f09
KY <rudyeon@naver.com>
2024-04-12 13:13:07 +0900

    [design] goods 구매창

--- app/_components/goods/Goods.tsx
+++ app/_components/goods/Goods.tsx
@@ -21,7 +21,7 @@ const Goods = () => {
         <div className="desktop:gap-[23px] laptop:gap-[20px] grid desktop:grid-cols-5 laptop:grid-cols-3">
           {goods?.map((item) => {
             return (
-              <div>
+              <div className="relative">
                 <img
                   alt="Card background"
                   className="rounded-2xl desktop:h-[494px] laptop:h-[431px]"
