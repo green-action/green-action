1afe55d59ccb693376a54f75dd964ac7a406b2e9
innes-k <innes.kwak@gmail.com>
2024-04-18 00:34:54 +0900

    [chore] 주석 제거

--- app/_api/messages/groupChat-api.ts
+++ app/_api/messages/groupChat-api.ts
@@ -75,62 +75,6 @@ export const insertNewParticipant = async ({
   }
 };
 
-// 채팅방 인원 파악 + 모집인원 파악
-// '채팅인원 === 모집인원' 된 경우 -> 모집상태 '모집마감'으로 변경
-// export const countParticipants = async ({
-//   room_id,
-//   action_id,
-// }: {
-//   room_id: string;
-//   action_id: string;
-// }) => {
-//   try {
-//     // 채팅방 인원 파악
-//     const { data: participants, error: participantsNumberError } =
-//       await supabase
-//         .from("chat_participants")
-//         .select("id")
-//         .eq("room_id", room_id);
-
-//     if (participantsNumberError) {
-//       console.log("participantsNumberError", participantsNumberError.message);
-//       throw participantsNumberError;
-//     }
-
-//     const participantsNumber = participants.length;
-
-//     // 모집인원 파악
-//     const { data: recruiting, error: recruitingNumberError } = await supabase
-//       .from("chat_rooms_info")
-//       .select("individual_green_actions(recruit_number)")
-//       .eq("id", room_id);
-
-//     if (recruitingNumberError) {
-//       console.log("recruitingNumberError", recruitingNumberError.message);
-//       throw recruitingNumberError;
-//     }
-
-//     const recruitingNumber =
-//       recruiting[0].individual_green_actions?.recruit_number;
-
-//     // '채팅인원 === 모집인원' 된 경우 -> 모집상태 '모집마감'으로 변경
-//     if (participantsNumber === recruitingNumber) {
-//       const { error } = await supabase
-//         .from("individual_green_actions")
-//         .update({ is_recruiting: false })
-//         .eq("id", action_id);
-
-//       if (error) {
-//         console.log("error", error.message);
-//         throw error;
-//       }
-//     }
-//   } catch (error) {
-//     console.error("error >>", error);
-//     throw error;
-//   }
-// };
-
 // 채팅방 인원 파악
 export const countParticipants = async (room_id: string) => {
   try {
