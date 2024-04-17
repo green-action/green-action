1f9fd5a7750f83c76749042efd836d1937925f19
KY <rudyeon@naver.com>
2024-04-17 16:08:11 +0900

    [design] goods 페이지 모바일 반응형 디자인 적용

--- app/_components/goods/GoodsImg.tsx
+++ app/_components/goods/GoodsImg.tsx
@@ -22,15 +22,18 @@ const GoodsImg = ({
       <div className="relative" key={item.id}>
         <img
           alt="Card background"
-          className="rounded-2xl desktop:h-[494px] laptop:h-[431px] object-cover"
+          className="rounded-2xl desktop:h-[494px] laptop:h-[431px] h-[441.76px] object-cover"
           src={item.img_url}
         />
         <div
-          className="cursor-pointer absolute inset-0 bg-black opacity-5 rounded-2xl desktop:h-[494px] laptop:h-[431px]"
+          className="cursor-pointer absolute inset-0 bg-black opacity-5 rounded-2xl desktop:h-[494px] laptop:h-[431px] h-[441.76px]"
           onClick={handleToggleProductInfo}
         ></div>
         {/* 제품 이미지 하단 전체 */}
-        <div className="flex justify-between desktop:mt-[28px] desktop:mx-[33px] laptop:mt-[23px] laptop:mx-[22px]">
+        <div
+          className="flex justify-between desktop:mt-[28px] desktop:mx-[33px] desktop:mb-0
+        laptop:mt-[23px] laptop:mx-[22px] laptop:mb-[160px] mt-[20px] mx-[20px] mb-[47px]"
+        >
           <div className="text-[15px] ">
             <p>{item.product_name}</p>
             <p className="text-[#929292]">{item.point.toLocaleString()}P</p>
