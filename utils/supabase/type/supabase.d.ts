e3b62567f9facc0dbe0939fa29eac9a8d9d82733
innes-k <innes.kwak@gmail.com>
2024-04-17 13:00:07 +0900

    [fix] goods - supabase 타입에러 해결

--- utils/supabase/type/supabase.d.ts
+++ utils/supabase/type/supabase.d.ts
@@ -249,24 +249,24 @@ export type Database = {
       goods: {
         Row: {
           id: string;
-          img_url: string | null;
-          point: number | null;
-          product_info: string | null;
-          product_name: string | null;
+          img_url: string;
+          point: number;
+          product_info: string;
+          product_name: string;
         };
         Insert: {
           id?: string;
-          img_url?: string | null;
-          point?: number | null;
-          product_info?: string | null;
-          product_name?: string | null;
+          img_url?: string;
+          point?: number;
+          product_info?: string;
+          product_name?: string;
         };
         Update: {
           id?: string;
-          img_url?: string | null;
-          point?: number | null;
-          product_info?: string | null;
-          product_name?: string | null;
+          img_url?: string;
+          point?: number;
+          product_info?: string;
+          product_name?: string;
         };
         Relationships: [];
       };
