3f45ffdb9981b7242da657e82de431a0fa827483
KY <rudyeon@naver.com>
2024-04-17 11:03:08 +0900

    [design] about 페이지 수정

--- app/_components/goods/ProductInfoModal.tsx
+++ app/_components/goods/ProductInfoModal.tsx
@@ -28,8 +28,8 @@ const ProductInfoModal = ({
 }) => {
   const queryClient = useQueryClient();
   const session = useSession();
-  // const loggedInUserUid = session.data?.user.user_uid;
-  const loggedInUserUid = "40056464-b704-4c3e-8821-584424005432";
+  const loggedInUserUid = session.data?.user.user_uid;
+  // const loggedInUserUid = "40056464-b704-4c3e-8821-584424005432";
 
   // const [showProductInfo, setShowProductInfo] = useState(false);
   const [confirmPurchase, setConfirmPurchase] = useState(false);
