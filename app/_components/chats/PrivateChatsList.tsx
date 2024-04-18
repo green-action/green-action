8727542767785f8283454d9d7a6e44e631dea1cf
innes-k <innes.kwak@gmail.com>
2024-04-19 00:09:17 +0900

    [chore] 주석 삭제

--- app/_components/chats/PrivateChatsList.tsx
+++ app/_components/chats/PrivateChatsList.tsx
@@ -1,6 +1,19 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { supabase } from "@/utils/supabase/client";
+
+import {
+  QUERY_KEY_PRIVATE_CHATS_LIST,
+  QUERY_KEY_PRIVATE_ROOM_IDS,
+} from "@/app/_api/queryKeys";
+
+import {
+  getPrivateChatsList,
+  getPrivateRoomIds,
+} from "@/app/_api/messages/privateChat-api";
+
 import { useResponsive } from "@/app/_hooks/responsive";
 
 import {
@@ -10,16 +23,6 @@ import {
   Button,
   Avatar,
 } from "@nextui-org/react";
-import {
-  getPrivateChatsList,
-  getPrivateRoomIds,
-} from "@/app/_api/messages/privateChat-api";
-import { supabase } from "@/utils/supabase/client";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import {
-  QUERY_KEY_PRIVATE_CHATS_LIST,
-  QUERY_KEY_PRIVATE_ROOM_IDS,
-} from "@/app/_api/queryKeys";
 
 const PrivateChatsList = ({
   onClose,
@@ -29,8 +32,6 @@ const PrivateChatsList = ({
   action_id: string;
 }) => {
   const { isDesktop, isLaptop, isMobile } = useResponsive();
-  // const [roomIds, setRoomIds] = useState<string[]>([]);
-
   const queryClient = useQueryClient();
 
   // 채팅방 room_id 배열 가져오기
@@ -38,52 +39,16 @@ const PrivateChatsList = ({
     queryKey: [QUERY_KEY_PRIVATE_ROOM_IDS],
     queryFn: async () => {
       const response = await getPrivateRoomIds(action_id);
-      // setRoomIds(response);
       return response;
     },
   });
 
-  // console.log("roomIds", roomIds);
-  // console.log("test", test);
-
   useEffect(() => {
-    // const fetchData = async () => {
-    //   const response = await getPrivateRoomIds(action_id);
-    //   return setRoomIds(response);
-    // };
-
-    // fetchData();
-
-    // const handleNewChatRoom = (payload: any) => {
-    //   fetchData();
-    // };
-
-    // // 채팅내용 구독 - room_id 별로 채팅내용 변경사항 구독
-    // const subscriptions = roomIds.map((roomId) => {
-    //   const subscription = supabase
-    //     .channel(`${roomId}`)
-    //     .on(
-    //       "postgres_changes",
-    //       { event: "INSERT", schema: "public", table: "chat_messages" },
-    //       // 해당 채팅방 최신화 - 채팅 내용들 가져온 쿼리키 무효화
-    //       (payload) => {
-    //         queryClient.invalidateQueries({
-    //           // queryKey: [QUERY_KEY_PRIVATE_ROOMS_LIST],
-    //         });
-    //       },
-    //     )
-    //     .subscribe();
-
-    //   return subscription;
-    // });
-
-    // 채팅방 테이블 변경사항 구독 (새 채팅방 insert될때 채팅방 리스트 실시간 업데이트)
+    // 채팅방 테이블 변경사항 구독 - 새 채팅방 insert될때 채팅방 리스트 실시간 업데이트
     const chatRoomsSubscription = supabase.channel(`{${action_id}}`).on(
       "postgres_changes",
       { event: "INSERT", schema: "public", table: "chat_rooms_info" },
-      // (payload) => handleNewChatRoom(payload),
 
-      // // 채팅방 리스트 무효화
       (payload) => {
         queryClient.invalidateQueries({
           queryKey: [QUERY_KEY_PRIVATE_ROOM_IDS],
@@ -92,13 +57,9 @@ const PrivateChatsList = ({
     );
 
     return () => {
-      // subscriptions.map((subscription) => {
-      //   return subscription.unsubscribe();
-      // });
       chatRoomsSubscription.unsubscribe();
     };
-    // 새로운 채팅방이 insert됐을때 useEffect 다시 실행 -> 다시 map이 돌면서 새 채팅방도 구독해준다.
-  }, []);
+  }, [action_id]);
 
   useEffect(() => {
     // 채팅내용 구독 - room_id 별로 채팅내용 변경사항 구독
@@ -108,10 +69,10 @@ const PrivateChatsList = ({
         .on(
           "postgres_changes",
           { event: "INSERT", schema: "public", table: "chat_messages" },
-          // 해당 채팅방 최신화 - 채팅 내용들 가져온 쿼리키 무효화
+
           (payload) => {
             queryClient.invalidateQueries({
-              // queryKey: [QUERY_KEY_PRIVATE_ROOMS_LIST],
+              queryKey: [QUERY_KEY_PRIVATE_CHATS_LIST],
             });
           },
         )
@@ -126,17 +87,6 @@ const PrivateChatsList = ({
       });
     };
   }, [roomIds]);
-
-  // // 채팅방 room_id 배열 가져오기
-  // const { data, error } = useQuery({
-  //   queryKey: [QUERY_KEY_PRIVATE_ROOM_IDS],
-  //   queryFn: async () => {
-  //     const response = await getPrivateRoomIds(action_id);
-  //     setRoomIds(response);
-  //     console.log("roomIds", roomIds);
-  //     return response;
-  //   },
-  // });
 
   // 채팅방 리스트 가져오기
   const { data: privateChatsList, error: privateChatListError } = useQuery({
@@ -156,7 +106,7 @@ const PrivateChatsList = ({
 
   console.log("privateChatsList", privateChatsList);
 
-  // 해당 읽었는지 안읽었는지는 chat_messages에 column을 하나 더 추가해야할듯 (isRead)
+  // TODO 해당 읽었는지 안읽었는지는 chat_messages에 column을 하나 더 추가해야할듯 (isRead)
   // 읽었는지 안읽었는지를 어떻게 파악하는건지??
 
   return (
