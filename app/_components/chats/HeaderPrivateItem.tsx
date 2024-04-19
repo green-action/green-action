25756c682cd0029da9152354dd7d7091e8ee1bba
innes-k <innes.kwak@gmail.com>
2024-04-19 23:11:48 +0900

    [feat] 헤더 1:1채팅 목록 - item별 데이터 반영

--- app/_components/chats/HeaderPrivateItem.tsx
+++ app/_components/chats/HeaderPrivateItem.tsx
@@ -10,21 +10,29 @@ const HeaderPrivateItem = ({ eachRoomInfo }: any) => {
     >
       <div className="flex mb-3">
         <span>green-action :</span>
-        <span>액션 이름</span>
+        <span>{eachRoomInfo.action_info.action_title}</span>
       </div>
       <div className="flex">
         <div>
-          <Avatar showFallback src="" alt="defaultImg" className="mr-5" />
+          <Avatar
+            showFallback
+            src={eachRoomInfo.message.user.profile_img}
+            alt="defaultImg"
+            className="mr-5"
+          />
         </div>
         <div className="w-full">
           <div className="flex flex-col gap-2">
             <div className="flex justify-between">
-              <span>상대방 닉네임</span>
-              <span>운영중</span>
+              <span>닉네임 : {eachRoomInfo.message.user.display_name}</span>
+              <span>
+                {eachRoomInfo.chat_rooms_info.participant_type === "방장" &&
+                  "운영중"}
+              </span>
             </div>
             <div className="flex justify-between">
-              <span>마지막 메시지</span>
-              <span>시간</span>
+              <span>{eachRoomInfo.message.content}</span>
+              <span>{eachRoomInfo.message.created_at}</span>
             </div>
           </div>
         </div>
