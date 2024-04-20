610aabb14104aeff745aea527580651d1001222e
Hyeryeon-Seo <0168968060@naver.com>
2024-04-21 01:12:46 +0900

    [fix] 배포버전 카카오맵 에러로 해결 중

--- app/_components/individualAction-add/FirstInputBox.tsx
+++ app/_components/individualAction-add/FirstInputBox.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect } from "react";
 import SearchAddressModal from "../daumPostCode/SearchAddressModal";
 import SearchMapModal from "../kakaoMap/SearchMapModal";
