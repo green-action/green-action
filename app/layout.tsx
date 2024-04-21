2fdf9950003fde81f84762755a7834f503cb200f
Hyeryeon-Seo <0168968060@naver.com>
2024-04-21 22:13:40 +0900

    [fix] 배포시 카카오맵 에러 해결 중 - api key 하나로 합치기

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
     <html lang="ko" className={GeistSans.className}>
       <body className="text-foreground">
         <Script
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services,clusterer&autoload=false`}
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&libraries=services,clusterer&autoload=false`}
           strategy="beforeInteractive"
         />
         <QueryProvider>
