647789b1cf1b4852c10ff62f4e6996abe2d00c08
innes-k <innes.kwak@gmail.com>
2024-04-25 00:54:59 +0900

    [design] 헤더 그룹챗 리스트 - 아이템 laptop 완성

--- app/_components/chats/HeaderGroupItem.tsx
+++ app/_components/chats/HeaderGroupItem.tsx
@@ -13,6 +13,7 @@ import GroupChatRoom from "./GroupChatRoom";
 import personIcon from "/app/_assets/image/logo_icon/icon/mypage/person.png";
 import { MODE_TODAY } from "@/app/_api/constant";
 import { previousFormatDate, todayFormatTime } from "@/utils/date/date";
+import { useResponsive } from "@/app/_hooks/responsive";
 
 const HeaderGroupItem = ({
   room_id,
@@ -23,6 +24,7 @@ const HeaderGroupItem = ({
 }) => {
   const session = useSession();
   const loggedInUserUid = session.data?.user.user_uid || "";
+  const { isDesktop, isLaptop, isMobile } = useResponsive();
 
   // 단체 채팅방 모달창
   const {
@@ -78,7 +80,13 @@ const HeaderGroupItem = ({
   return (
     <div
       key={room_id}
-      className="flex flex-col bg-white mr-3 mb-6 cursor-pointer rounded-2xl px-9 py-8"
+      className={`flex flex-col bg-white cursor-pointer rounded-2xl ${
+        isDesktop
+          ? "mb-6 px-9 py-8"
+          : isLaptop
+          ? "mb-4 px-6 py-5"
+          : isMobile && "mb-3 px-4 py-2"
+      }`}
       onClick={() => {
         onGroupChatOpen();
       }}
@@ -88,16 +96,47 @@ const HeaderGroupItem = ({
           <img
             src={actionInfo?.action_url}
             alt="defaultImg"
-            className="mr-10 w-[65px] h-[65px] rounded-full"
+            // className="mr-10 w-[65px] h-[65px] "
+            className={`rounded-full ${
+              isDesktop
+                ? "w-[65px] h-[65px] mr-10"
+                : isLaptop
+                ? "w-[50px] h-[50px] mr-7"
+                : isMobile && "w-[30px] h-[30px] mr-3"
+            }`}
           />
         </div>
-        <div className="flex flex-col gap-2 w-full">
+        <div
+          className={`flex flex-col w-full ${
+            isDesktop ? "gap-2" : isLaptop ? "gap-1" : isMobile && "gap-1"
+          }`}
+        >
           <div className="flex justify-between">
-            <div className="flex items-center gap-2">
-              <p className="bg-[#7B7B7B] px-3 rounded-xl text-white">
+            <div
+              className={`flex items-center ${
+                isDesktop ? "gap-2" : isLaptop ? "gap-2" : isMobile && "gap-1"
+              }`}
+            >
+              <p
+                className={`bg-[#7B7B7B] rounded-xl text-white ${
+                  isDesktop
+                    ? "px-3"
+                    : isLaptop
+                    ? "text-xs px-2 py-[1px]"
+                    : isMobile && "text-[10px] px-1"
+                }`}
+              >
                 {loggedInUserUid === actionInfo?.user_uid && "My"}
               </p>
-              <span className="font-black text-xl max-w-[200px] overflow-hidden whitespace-nowrap overflow-ellipsis">
+              <span
+                className={`font-black max-w-[200px] overflow-hidden whitespace-nowrap overflow-ellipsis ${
+                  isDesktop
+                    ? "text-xl"
+                    : isLaptop
+                    ? "text-sm"
+                    : isMobile && "text-xs"
+                }`}
+              >
                 {actionInfo?.title}
               </span>
             </div>
@@ -105,9 +144,19 @@ const HeaderGroupItem = ({
               <Image
                 src={personIcon}
                 alt="person-icon"
-                className="w-[20px] h-[20px] text-gray-600"
+                className={`text-gray-600 ${
+                  isDesktop
+                    ? "w-[20px] h-[20px]"
+                    : isLaptop
+                    ? "w-[17px] h-[17px]"
+                    : isMobile && ""
+                }`}
               />
-              <span className="text-[#7B7B7B]">
+              <span
+                className={`text-[#7B7B7B] ${
+                  isLaptop ? "text-sm" : isMobile && ""
+                }`}
+              >
                 {participantsCount} / {actionInfo?.recruit_number}
               </span>
             </div>
@@ -115,15 +164,33 @@ const HeaderGroupItem = ({
           <div className="flex justify-between">
             <div className="max-w-[170px] text-gray-500 overflow-hidden whitespace-nowrap overflow-ellipsis">
               {lastMessageInfo?.content ? (
-                <span className="max-w-[200px] text-gray-500 overflow-hidden whitespace-nowrap overflow-ellipsis">
+                <span
+                  className={`text-gray-500 overflow-hidden whitespace-nowrap overflow-ellipsis ${
+                    isDesktop
+                      ? "max-w-[200px]"
+                      : isLaptop
+                      ? "max-w-[170px] text-sm"
+                      : isMobile && "max-w-[120px] text-xs"
+                  }`}
+                >
                   {lastMessageInfo?.content}
                 </span>
               ) : (
-                <span className="text-gray-500">(아직 메시지가 없습니다.)</span>
+                <span
+                  className={`text-gray-500 ${
+                    isLaptop ? "text-sm" : isMobile && "text-xs"
+                  }`}
+                >
+                  (아직 메시지가 없습니다.)
+                </span>
               )}
             </div>
             <div className="flex items-center gap-2">
-              <span className="text-gray-500">
+              <span
+                className={`text-gray-500 ${
+                  isLaptop ? "text-sm" : isMobile && "text-xs"
+                }`}
+              >
                 {mode === MODE_TODAY
                   ? todayFormatTime(lastMessageInfo.created_at)
                   : previousFormatDate(lastMessageInfo.created_at)}
