6f608aa40ed4f8a762bd44c714967afc0f446a54
HY965 <hyeonj965@gmail.com>
2024-04-26 15:23:37 +0900

    [design]about 글 수정

--- app/_components/about/AboutComputerSize.tsx
+++ app/_components/about/AboutComputerSize.tsx
@@ -20,25 +20,25 @@ const AboutComputerSize = () => {
             alt="about title image"
             className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[95%] object-cover mx-auto rounded-b-[60px] brightness-[.4] desktop:h-[1090px] desktop:mb-[282px] laptop:h-[1000px] laptop:mb-[282px]"
           />
-          <div className="z-0 flex flex-col items-center text-white text-[64px] font-thin desktop:mt-[300px] laptop:w-[100%] laptop:mt-[335px]">
-            <p className="font-['Italiana']">
-              {isDesktop
-                ? "Experience the earth breathing"
-                : "Experience the earth"}
+          <div className="z-0 flex flex-col items-center text-white text-[59px] font-semibold desktop:mt-[300px] laptop:w-[100%] laptop:mt-[335px]">
+            {/* <p className="font-['Italiana']"> */}
+            <p className="font-['Pretendard']">
+              {isDesktop ? "환경을 위한 실천," : "환경을 위한 실천,"}
             </p>
-            <p className="font-['Italiana']">
-              {isDesktop
-                ? "together in your daily life"
-                : "breathing together in your"}
+            <p className="font-['Pretendard']">
+              {isDesktop ? "내일을 향한 변화, 숨" : "내일을 향한 변화, 숨"}
             </p>
-            {isLaptop && <p className="font-['Italiana']"> daily life</p>}
+            {/* {isLaptop && <p className="font-['Italiana']"> daily life</p>} */}
           </div>
         </div>
         <div className="flex flex-col items-center desktop:mt-[700px] laptop:mt-[505px]">
           {/* second part - 설명글 */}
           <div className="flex flex-col justify-center text-center  mx-auto font-bold desktop:text-[36px] desktop:mb-[420px] desktop:w-[1426px] laptop:text-[30px] laptop:mb-[330px] w-[852px]">
+            <p>나의그린 라이프 실천 내역을 한눈에 보고 한번에 찾으세요.</p>
+            <p>환경을 위한 첫걸음을 함께 쉽고 재밌게 할 수 있는 서비스</p>
+            <p>숨과 함께라면 당신의 일상이 새로워질 거예요.</p>
             {/* 첫 줄 */}
-            <div>
+            {/* <div>
               <p className="inline-block text-with-background bg-text-bg-1 bg-cover bg-center rounded-full text-white mr-2 desktop:pt-1 desktop:w-[444px] desktop:h-[60px] laptop:w-[380px] laptop:h-[47px]">
                 '일상에서 쉽게 실천할 수 있는
               </p>
@@ -49,9 +49,9 @@ const AboutComputerSize = () => {
                   : `환경 캠페인'은`}
               </p>
               {isLaptop && <p className="inline-block">지구를 지키는</p>}
-            </div>
+            </div> */}
             {/* 두번째 줄 */}
-            <div>
+            {/* <div>
               <p className="inline-block">
                 {isDesktop
                   ? "당신의 일상에서 환경을 지키는"
@@ -63,9 +63,9 @@ const AboutComputerSize = () => {
                   ? "작은 습관들이 모여 큰 변화를"
                   : "습관들이 모여 큰 변화를"}
               </p>
-              <p className="inline-block">이끌어낼 수 있습니다.</p>
-              {/* 3~4번째 줄 */}
-              <p className="inline-block">
+              <p className="inline-block">이끌어낼 수 있습니다.</p> */}
+            {/* 3~4번째 줄 */}
+            {/* <p className="inline-block">
                 {isDesktop
                   ? `우리의 목표는 모든 사람이 쉽게 실천할 수 있는 환경 보호 방법을
             제시하고, 그것들을 실제 행동으로
@@ -88,9 +88,9 @@ const AboutComputerSize = () => {
                   </p>
                 </>
               )}
-            </div>
+            </div> */}
             {/* 마지막 2문장 */}
-            <div>
+            {/* <div>
               <p className="inline-block">지금 바로</p>
               <p className="inline-block text-with-background bg-text-bg-3 bg-cover bg-center rounded-full text-white desktop:pt-1 mx-2 desktop:w-[370px] desktop:h-[60px]">
                 '지구가 숨쉬다. SOOM'
@@ -110,7 +110,7 @@ const AboutComputerSize = () => {
                   당신의 작은 노력이 큰 변화를 만들어낼 수 있습니다. 함께해요!
                 </p>
               )}
-            </div>
+            </div> */}
           </div>
         </div>
         {/* third part - 카드 4장 */}
@@ -119,13 +119,19 @@ const AboutComputerSize = () => {
             <div className={`flex ${isLaptop && "justify-end"}`}>
               {/* 카드 1 */}
               <div className="flex flex-col justify-between bg-white rounded-[40px] border-1.5 desktop:w-[450px] desktop:h-[590px] desktop:mb-[170px] desktop:mr-[40px] desktop:py-[45px] desktop:px-[60px] laptop:w-[434px] laptop:h-[572px] laptop:mb-[173px] laptop:mr-[18px] laptop:py-[45px] laptop:px-[60px]">
-                <span className="font-[Italiana] text-[40px] text-[#3A3A3A] ">
-                  (1) Join us
-                </span>
-                <span className="text-[#3A3A3A] text-[16px] font-[Pretendard-ExtraLight]">
-                  첫 방문이라면, 회원가입하기. 하루 1번 로그인을
+                <div className="flex flex-col">
+                  <span className="flex gap-2 text-[#BFBFBF] text-[18px]">
+                    함께 실천 <p className="text-[#3A3A3A]">시작하기</p>
+                  </span>
+
+                  <span className="font-[Italiana] text-[40px] text-[#3A3A3A] mt-5">
+                    (1) Join us
+                  </span>
+                </div>
+                <span className="text-[#646464] desktop:text-[22px] laptop:text-[20px] mt-56 mb-5">
+                  첫 방문이라면, 회원가입 후 로그인
                   <br />
-                  하면 포인트를 적립할 수 있습니다.
+                  하면 포인트를 적립할 수 있어요.
                 </span>
               </div>
               {/* 카드 2 */}
@@ -136,13 +142,20 @@ const AboutComputerSize = () => {
                   className="w-full h-full object-cover rounded-[40px] brightness-[.8]"
                 />
                 <div className="absolute flex flex-col justify-between inset-0 desktop:top-[53px] desktop:bottom-[53px] desktop:left-[60px] laptop:top-[45px] laptop:bottom-[45px] laptop:left-[50px]">
-                  <span className="font-[Italiana] text-[40px] text-white">
-                    (2) Take
-                    <br /> green-action
-                  </span>
-                  <span className="text-white text-[16px] font-[Pretendard-ExtraLight]">
-                    본인이 직접 실천 캠페인을 열어 함께 행동해보세요.
-                    <br /> 글 업로드시 포인트를 적립할 수 있습니다.
+                  <div className="flex flex-col">
+                    <span className="flex gap-2 text-[#BFBFBF] text-[18px]">
+                      함께 실천 <p className="text-white">실행하기</p>
+                    </span>
+                    {/* font-[Pretendard-ExtraLight] */}
+                    <span className="font-[Italiana] text-[40px] text-white mt-5">
+                      (2) Take
+                      <br /> green-action
+                    </span>
+                  </div>
+                  <span className="text-white desktop:text-[22px] laptop:text-[20px]">
+                    본인이 직접 실천 캠페인을 열어 함께
+                    <br /> 행동해보세요. 글 업로드 하여 포인트
+                    <br />를 받아보세요.
                   </span>
                 </div>
               </div>
@@ -156,40 +169,39 @@ const AboutComputerSize = () => {
                   className="w-full h-full object-cover rounded-[40px] brightness-[.8]"
                 />
                 <div className="absolute flex flex-col justify-between inset-0 desktop:top-[53px] desktop:bottom-[53px] desktop:left-[60px] laptop:top-[45px] laptop:bottom-[45px] laptop:left-[54px]">
-                  <span className="font-[Italiana] text-[40px] text-white">
-                    {isDesktop ? (
-                      <>
-                        (3) Share your
-                        <br />
-                        experience
-                      </>
-                    ) : (
-                      <>
-                        (3) Share
-                        <br />
-                        your
-                        <br />
-                        experience
-                      </>
-                    )}
-                  </span>
-                  <span className="text-white text-[16px] font-[Pretendard-ExtraLight]">
-                    green-action을 실천했다면, community에 글을
-                    <br /> 업로드해보세요. 또한, 다른 greener의 게시글에
-                    <br /> 댓글을 달면 포인트를 포인트를 드립니다.
+                  <div className="flex flex-col">
+                    <span className="flex gap-2 text-[#BFBFBF] text-[18px]">
+                      함께 실천 <p className="text-white">나누기</p>
+                    </span>
+                    <span className="font-[Italiana] text-[40px] text-white mt-5">
+                      (3) Share
+                      <br />
+                      experience
+                    </span>
+                  </div>
+                  <span className="text-white desktop:text-[22px] laptop:text-[20px]">
+                    일상에서 실천한 글을 업로드 해보세요.
+                    <br /> 다른 greener의 게시글에 댓글을 달아
+                    <br /> 포인트를 받아보세요.
                   </span>
                 </div>
               </div>
               {/* 카드 4 */}
               <div className="flex flex-col justify-between bg-white border-1.5 rounded-[40px] desktop:w-[450px] desktop:h-[590px]  desktop:py-[53px] desktop:px-[60px] laptop:w-[434px] laptop:h-[572px] laptop:py-[45px] laptop:px-[54px]">
-                <span className="font-[Italiana] text-[40px] text-[#3A3A3A]">
-                  (4) Practice in <br />
-                  daily life
-                </span>
-                <span className="text-[#3A3A3A] text-[16px] font-[Pretendard-ExtraLight]">
-                  일상에서 꾸준히 실천한 당신! 자동으로 쌓인
+                <div className="flex flex-col">
+                  <span className="flex gap-2 text-[#BFBFBF] text-[18px]">
+                    함께 실천 <p className="text-[#3A3A3A]">지속하기</p>
+                  </span>
+                  <span className="font-[Italiana] text-[40px] text-[#3A3A3A] mt-5">
+                    (4) Practice in <br />
+                    daily life
+                  </span>
+                </div>
+                <span className="text-[#646464]  desktop:text-[22px] laptop:text-[20px] mt-55">
+                  일상에서 꾸준히 실천하며 쌓은 포인트
                   <br />
-                  포인트로 친환경 소재의 굿즈를 구매할 수 있습니다.
+                  로 친환경 소재로 만든 숨 굿즈를 구매
+                  <br />할 수 있어요.
                 </span>
               </div>
             </div>
@@ -203,14 +215,14 @@ const AboutComputerSize = () => {
             alt="explore more image"
             className="mx-auto rounded-[50px] mb-[40px] object-cover desktop:w-[1844px] desktop:h-[882px] laptop:w-[955px] laptop:h-[879px]"
           />
-          <span className="absolute font-[Italiana] text-white text-[48px] inset-0 top-[350px]">
-            Experience the earth breathing together <br />
-            in your daily life
+          <span className="absolute font-semibold text-white desktop:text-[59px] laptop:text-[48px] inset-0 desktop:top-[300px] laptop:top-[350px]">
+            환경을 위한 첫걸음 <br />
+            숨에서 쉽고 재밌게
           </span>
-          <div className="absolute inset-x-0 bottom-[320px] flex items-center justify-center text-white text-[20px] font-[Inter]">
+          <div className="absolute inset-x-0 bottom-[290px] flex items-center justify-center text-white text-[20px] font-[Inter]">
             <Link
               href="/individualAction"
-              className="flex items-center justify-center rounded-full border-1 border-white w-[200px] h-[50px]"
+              className="flex items-center justify-center rounded-full border-1 border-white desktop:w-[200px] laptop:w-[168px] h-[50px] laptop:text-[15px]"
             >
               Explore more
             </Link>
--- app/_components/about/AboutMobileSize.tsx
+++ app/_components/about/AboutMobileSize.tsx
@@ -19,40 +19,44 @@ const AboutMobileSize = () => {
             className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[360px] h-[598px] object-cover mx-auto mb-[282px] brightness-[.4]"
           />
 
-          <div className="z-0 mt-[240px] flex flex-col w-[280px] items-center text-white text-[32px] font-thin">
-            <p className="font-['Italiana']">Experience the earth</p>
-            <p className="font-['Italiana']">breathing together</p>
-            <p className="font-['Italiana']">in your daily life</p>
+          <div className="z-0 mt-[240px] flex flex-col w-[280px] items-center text-white text-[32px] font-semibold">
+            <p className="">환경을 위한 실천,</p>
+            <p className="">내일을 향한 변화, 숨</p>
           </div>
         </div>
-        <div className="mt-[240px] mb-[205px] flex flex-col items-center">
+        <div className="mt-[350px] mb-[205px] flex flex-col items-center">
           {/* second part - 설명글 */}
-          <div className="w-[300px] flex flex-col justify-center text-center text-[16px] font-semibold">
-            {/* 첫 줄 */}
-            <div>
+          <div className="w-[300px] flex flex-col justify-center text-center text-[20px] font-medium">
+            <p>나의그린 라이프 실천 내역을</p>
+            <p>한눈에 보고 한번에 찾으세요.</p>
+            <p>환경을 위한 첫걸음을 함께 쉽고 </p>
+            <p>재밌게 할 수 있는 서비스.</p>
+            <p>숨과 함께라면 당신의 일상이</p>
+            <p>새로워질 거예요.</p>
+            {/* <div>
               <p className="inline-block text-with-background bg-text-bg-1 bg-cover bg-center rounded-full text-white w-[195px] h-[28px] mr-1">
                 '일상에서 쉽게 실천할 수 있는
               </p>
               <p className="inline-block mr-2">환경 캠페인'은</p>
-            </div>
+            </div> */}
             {/* 2-3 줄 */}
-            <div>
+            {/* <div>
               <p className="inline-block">
                 지구를 지키는 우리의 작은 노력으로부터
               </p>
               <p className="inline-block">
                 출발합니다. 당신의 일상에서 환경을 지키는
-              </p>
+              </p> */}
 
-              {/* 4 줄 */}
-              <p className="inline-block mr-1">작은</p>
+            {/* 4 줄 */}
+            {/* <p className="inline-block mr-1">작은</p>
               <p className="inline-block text-with-background bg-text-bg-2 bg-cover bg-center rounded-full text-white w-[165px] h-[28px] mr-1">
                 습관들이 모여 큰 변화를
               </p>
               <p className="inline-block">이끌어낼 수</p>
-            </div>
+            </div> */}
             {/* 마지막 2문장 */}
-            <div>
+            {/* <div>
               <p className="inline-block">
                 있습니다. 이제 우리의 캠페인에 참여하여
               </p>
@@ -81,20 +85,26 @@ const AboutMobileSize = () => {
               </p>
               <p className="inline-block">당신의 작은 노력이 큰 변화를</p>
               <p className="inline-block">만들어낼 수 있습니다. 함께해요!</p>
-            </div>
+            </div> */}
           </div>
         </div>
         {/* third part - 카드 4장 */}
         <div className="mx-auto w-[265px] h-[1445px] mb-[147px]">
           {/* 360 카드 1 */}
           <div className="border-1.5 flex flex-col h-[346px] mb-[17px] justify-between bg-white rounded-[40px] py-[28px] px-[33px]">
-            <span className="font-[Italiana] text-[32px] text-[#3A3A3A]">
-              (1) Join us
-            </span>
-            <span className="text-[#3A3A3A] text-[13px] font-[Pretendard-ExtraLight]">
-              첫 방문이라면, 회원가입하기. 하루 <br />
-              1번 로그인을 하면 포인트를 적립
-              <br /> 할 수 있습니다.
+            <div className="flex flex-col">
+              <span className="flex gap-1 text-[#BFBFBF] text-[13px]">
+                함께 실천 <p className="text-[#3A3A3A]">시작하기</p>
+              </span>
+
+              <span className="font-[Italiana] text-[32px] text-[#3A3A3A] mt-3">
+                (1) Join us
+              </span>
+            </div>
+            <span className="text-[#646464] text-[14px] mb-2">
+              첫 방문이라면, 회원가입 후 로그인
+              <br />
+              하면 포인트를 적립할 수 있어요.
             </span>
           </div>
           {/* 360 카드 2 */}
@@ -109,14 +119,19 @@ const AboutMobileSize = () => {
               className="w-full h-full object-cover rounded-[40px] brightness-[.8]"
             />
             <div className="absolute flex flex-col justify-between inset-0 top-[24px] bottom-[32px] left-[32px]">
-              <span className="font-[Italiana] text-[32px] text-white">
-                (2) Take
-                <br /> green-action
-              </span>
-              <span className="text-white text-[13px] font-[Pretendard-ExtraLight]">
+              <div className="flex flex-col">
+                <span className="flex gap-1 text-[#BFBFBF] text-[13px]">
+                  함께 실천 <p className="text-white">실행하기</p>
+                </span>
+                <span className="font-[Italiana] text-[32px] text-white mt-3">
+                  (2) Take
+                  <br /> green-action
+                </span>
+              </div>
+              <span className="text-white text-[14px]">
                 본인이 직접 실천 캠페인을 열어 함께
-                <br /> 행동해보세요. 글 업로드시 포인트를
-                <br /> 적립할 수 있습니다.
+                <br /> 행동해보세요. 글 업로드 하여 포인트
+                <br />를 받아보세요.
               </span>
             </div>
           </div>
@@ -132,31 +147,38 @@ const AboutMobileSize = () => {
               className="w-full h-full object-cover rounded-[40px] brightness-[.8]"
             />
             <div className="absolute flex flex-col justify-between inset-0 top-[27px] bottom-[27px] left-[30px]">
-              <span className="font-[Italiana] text-[32px] text-white">
-                (3) Share <br />
-                your
-                <br /> experience
-              </span>
-              <span className="text-white text-[13px] font-[Pretendard-ExtraLight]">
-                Green-action을 실천했다면,
-                <br /> community에 글을 업로드해보세요.
-                <br /> 또한, 다른 greener의 게시글에 댓글을
-                <br />
-                달면 포인트를 드립니다.
+              <div className="flex flex-col">
+                <span className="flex gap-2 text-[#BFBFBF] text-[13px]">
+                  함께 실천 <p className="text-white">나누기</p>
+                </span>
+                <span className="font-[Italiana] text-[32px] text-white mt-3">
+                  (3) Share
+                  <br /> experience
+                </span>
+              </div>
+              <span className="text-white text-[14px]">
+                일상에서 실천한 글을 업로드 해보세요.
+                <br /> 다른 greener의 게시글에 댓글을 달아
+                <br /> 포인트를 받아보세요.
               </span>
             </div>
           </div>
           {/* 360 카드 4 */}
           <div className="border-1.5 flex flex-col h-[346px] mb-[17px] justify-between bg-white rounded-[40px] py-[28px] px-[33px]">
-            <span className="font-[Italiana] text-[32px] text-[#3A3A3A]">
-              (4) <br /> Practice <br />
-              in daily <br /> life
-            </span>
-            <span className="text-[#3A3A3A] text-[13px] font-[Pretendard-ExtraLight]">
-              일상에서 꾸준히 실천한 당신. <br />
-              자동으로 쌓인 포인트로 친환경 소재의
+            <div className="flex flex-col">
+              <span className="flex gap-2 text-[#BFBFBF] text-[13px]">
+                함께 실천 <p className="text-[#3A3A3A]">지속하기</p>
+              </span>
+              <span className="font-[Italiana] text-[32px] text-[#3A3A3A] mt-3">
+                (4) <br /> Practice in <br />
+                daily life
+              </span>
+            </div>
+            <span className="text-[#646464] text-[13px] mb-2">
+              일상에서 꾸준히 실천하며 쌓은 포인트
               <br />
-              굿즈를 구매할 수 있습니다.
+              로 친환경 소재로 만든 숨 굿즈를 구매
+              <br />할 수 있어요.
             </span>
           </div>
         </div>
@@ -168,14 +190,13 @@ const AboutMobileSize = () => {
             alt="explore more image"
             className="w-[360px] h-[714px] mx-auto object-cover"
           />
-          <span className="absolute font-[Italiana] text-white text-[24px] inset-0 top-[295px]">
-            Experience the earth <br /> breathing together <br /> in your daily
-            life
+          <span className="absolute  text-white text-[36px] inset-0 top-[295px] font-semibold">
+            환경을 위한 첫걸음 <br /> 숨에서 쉽고 재밌게
           </span>
-          <div className="absolute inset-x-0 bottom-[205px] flex items-center justify-center text-white text-[15px] font-[Inter] font-semibold">
+          <div className="absolute inset-x-0 bottom-[220px] flex items-center justify-center text-white text-[15px] font-[Inter] font-semibold">
             <Link
               href="/individualAction"
-              className="flex items-center justify-center rounded-[24px] border-1 border-white w-[153px] h-[50px]"
+              className="flex items-center justify-center rounded-[24px] border-1 border-white w-[141px] h-[41px]"
             >
               Explore more
             </Link>
